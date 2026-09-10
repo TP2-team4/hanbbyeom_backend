9a5a42f84952568d826563c25d4e8e44ca940456
차원우 <wncha1202@gmail.com>
2026-09-10 17:50:57 +0900

    first commit

---
+++ src/main/java/com/team4/hanbbyeom/HanbbyeomApplication.java
@@ -0,0 +1,13 @@
+package com.team4.hanbbyeom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbbyeomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbbyeomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team4/hanbbyeom/HanbbyeomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.hanbbyeom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanbbyeomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
